37ccd2a2e3e9806572e1cd971c15f06b12e9a2d4
Paresh kamble <37034349+sparesh21@users.noreply.github.com>
2018-03-14 17:57:53 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
+import { routes } from './app.routing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './templates/home/home.component';
+import { AboutComponent } from './templates/about/about.component';
+import { ComingSoonComponent } from './templates/coming-soon/coming-soon.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ComingSoonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routes,
+	  AngularFontAwesomeModule,
+	  NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/js/custom.js
@@ -0,0 +1,11 @@
+/*
+Project Name : Reen Website;
+Author: Paresh Kamble;
+Developed By: Paresh Kamble;
+Rights: Reserved;
+Date: 14-03-2018
+
+*/
+$(document).ready(function() {
+	console.log('Project Name : Reen Website; Author: Paresh Kamble; Developed By: Paresh Kamble; Rights: Reserved; Date: 14-03-2018');
+});
