f9376cd600491eade648e96efeba050861970c03
Paresh kamble <37034349+sparesh21@users.noreply.github.com>
2018-03-19 20:07:39 +0530

    new section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  searchOpen: boolean = false;
+  searchIcon : string = "search";
+  
+  openSearch(){
+    if(this.searchOpen == false){
+      this.searchOpen = true;
+      this.searchIcon = "times"
+    }
+    else{
+      this.searchOpen = false;
+      this.searchIcon = "search"
+    }
+    
+  }
+
 }
--- src/app/templates/home/home.component.ts
+++ src/app/templates/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,14 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+	  var owl = $('.owl-carousel');
+	  
+	  owl.owlCarousel({
+		  margin: 30,
+		  nav: false,
+		  dots: true,
+		  items: 4
+	  });
   }
 
 }
